1c2431c6508e7033a48f91e0fb7f542e535553eb
Jordi Pujol-Ahulló <jpahullo@gmail.com>
2017-04-22 19:27:42 +0200

    version.php: adding maturity.

--- version.php
+++ version.php
@@ -6,4 +6,4 @@ $plugin->requires = 2012061700;  //Requires this Moodle version
 $plugin->component = 'repository_searchable'; //Full name of the plugin (used for diagnostics)
 $plugin->cron = 0; // How often to run automatic updates (0 to disable)
 $plugin->release = '0.1';
-
+$plugin->maturity = MATURITY_STABLE;
