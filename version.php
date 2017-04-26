0c80035743c7db234e5beb319083374d107bcbf8
Jordi Pujol-Ahulló <jpahullo@gmail.com>
2017-04-26 18:38:38 +0200

    upgrade to version 0.1.1.

--- version.php
+++ version.php
@@ -19,5 +19,5 @@ $plugin->version = 2017021700; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2012061700;  // Requires this Moodle version.
 $plugin->component = 'repository_searchable'; // Full name of the plugin (used for diagnostics).
 $plugin->cron = 0; // How often to run automatic updates (0 to disable).
-$plugin->release = '0.1';
+$plugin->release = '0.1.1';
 $plugin->maturity = MATURITY_STABLE;
