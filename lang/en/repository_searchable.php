96753eac2b9166c338e0ae4bd12fe9ed64299265
Jordi Pujol-Ahulló <jpahullo@gmail.com>
2017-05-24 20:24:14 +0200

    fix #6 - i18n strings are simple multiline strings.

--- lang/en/repository_searchable.php
+++ lang/en/repository_searchable.php
@@ -15,11 +15,11 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $string['pluginname'] = 'Searchable filesystem';
-$string['pluginname_help'] = 'Searchable filesystem repository helps to map' .
-        ' local server directories with so many (thousands of) files on it.' .
-        ' In that case, the default server repository hangs up and becomes not' .
-        ' usable. This plugin attemps to make affordable to map local directories' .
-        ' with lots of files, like the directory with Moodle course backups.';
+$string['pluginname_help'] = 'Searchable filesystem repository helps to map
+ local server directories with so many (thousands of) files on it.
+ In that case, the default server repository hangs up and becomes not
+ usable. This plugin attemps to make affordable to map local directories
+ with lots of files, like the directory with Moodle course backups.';
 $string['configplugin'] = 'Set up my searchable filesystem';
 $string['searchable:view'] = 'View the searchable filesystem';
 $string['keyword'] = 'Filename pattern';
